9f97c6d8bf80502ba24de126967e9921ac51ac2a
a.mikhalev <a.mikhalev@pflb.ru>
2016-03-29 17:28:49 +0300

    Add readme, small refactoring

---
+++ README.md
@@ -0,0 +1,5 @@
+Script that allows you to quickly upload pictures to imgur.com.
+
+Take the existant "Upload to Imgur" shortcut (or make your own for "launcher.bat") and copy it to "%USERPROFILE%\SendTo" folder.
+
+When the script passes, you will have a direct link to your uploaded image in the clipboard.
--- imgur_script.ps1
+++ imgur_script.ps1
@@ -30,7 +30,6 @@ $r = [System.Net.WebRequest]::Create("http://imgur.com/upload/start_session/");
 $resp = $r.GetResponse().GetResponseStream();
 $sr = new-object System.IO.StreamReader $resp;
 $result = $sr.ReadToEnd();
-# write-host $result
 
 $found = $result -match '"sid":"(.+?)"';
 if ($found) {
@@ -44,13 +43,11 @@ $sr = new-object System.IO.StreamReader $resp;
 $result = $sr.ReadToEnd();
 write-host($result);
 
-#post request
-
 $url='http://imgur.com/upload?current_upload=1&total_uploads=1&terms=0&gallery_type=&location=outside&gallery_submit=0&create_album=0&album_title=Optiona%20Album%20Title&sid={0}' -f $sid;
 
 $bytes = [System.IO.File]::ReadAllBytes($file);
 $enc = [System.Text.Encoding]::GetEncoding(0);
-$filedata = $enc.GetString($bytes);		# encoded data from file 
+$filedata = $enc.GetString($bytes);	
 
 $boundary = [System.Guid]::NewGuid().ToString();
 $header = "--{0}" -f $boundary;
@@ -63,10 +60,7 @@ $footer = "--{0}--" -f $boundary;
 [void]$contents.AppendLine('Content-Transfer-Encoding: binary');
 [void]$contents.AppendLine();
 [void]$contents.AppendLine($filedata);
-# [void]$contents.AppendLine("actual file content, not shown here");
 [void]$contents.AppendLine($footer);
-
-# write-host $contents
 
 $postContentType = "multipart/form-data; boundary={0}; charset=UTF-8" -f $boundary;
 
@@ -104,9 +98,9 @@ while ($true) {
 	$read = $gzipStream.Read($buffer, 0, 512);
 	if ($read -le 0) {
 		break;
-	}
+	};
 	$output.Write($buffer, 0, $read);
-}
+};
 $enc = [System.Text.Encoding]::GetEncoding(0);
 $result = $enc.GetString($output.ToArray());	
 write-host($result);
@@ -114,7 +108,7 @@ write-host($result);
 $found = $result -match '"hash":"(.+?)"'
 if ($found) {
     $hash = $matches[1];
-}
+};
 write-host('hash = {0}' -f $hash);
 
 $link = 'http://i.imgur.com/{0}{1}' -f $hash, $ext;
@@ -123,8 +117,4 @@ write-host;
 write-host($link);
 write-host;
 
-$link | C:\Windows\System32\clip.exe
-
-# powershell
-
-
+$link | C:\Windows\System32\clip.exe;
