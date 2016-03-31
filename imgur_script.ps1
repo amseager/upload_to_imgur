dd5f796f4e77c56cb07606e853cf990ace539eb0
a.mikhalev <a.mikhalev@pflb.ru>
2016-03-31 17:38:37 +0300

    Add group uploading

--- imgur_script.ps1
+++ imgur_script.ps1
@@ -2,15 +2,31 @@ Param(
 	[String]$files
 );
 
+write-host("You'll get a direct link to your image in clipboard when the script passes.");
+write-host("This window will close automatically. Please wait... ");
+write-host;
+
+
 $pattern = '(\w:.+?\.\w+)\s|(\w:.+?\.\w+)$';
 $filesArray = [regex]::matches($files, $pattern);
 $filesCount = $filesArray.count
 
 
+
 # 1st req - startsession
 
-$url = 'http://imgur.com/upload/start_session/';
+$url = 'http://www.imgur.com/upload/start_session/';
 $r = [System.Net.WebRequest]::Create($url);
+
+$r.KeepAlive="true";
+$r.Accept = "application/json, text/javascript, */*; q=0.01";
+$r.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36 OPR/36.0.2130.32";
+$r.Referer = "http://imgur.com/";
+
+$r.Headers.Add("Accept-Encoding", "gzip, deflate, lzma, sdch");
+$r.Headers.Add("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4");
+$r.Headers.Add("X-Requested-With", "XMLHttpRequest");
+
 $resp = $r.GetResponse().GetResponseStream();
 $sr = new-object System.IO.StreamReader $resp;
 $result = $sr.ReadToEnd();
@@ -19,7 +35,9 @@ $found = $result -match '"sid":"(.+?)"';
 if ($found) {
     $sid = $matches[1];
 };
-write-host('sid = {0}' -f $sid);
+# write-host 'sid = {0}' -f $sid;
+
+$cookieSid = 'IMGURSESSION={0}' -f $sid;
 
 
 
@@ -46,24 +64,19 @@ $r.Headers.Add("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4");
 $r.Headers.Add("X-Requested-With", "XMLHttpRequest");
 $r.Headers.Add("X-Imgur", "1");
 
+$r.Headers.Add("Cookie", $cookieSid);
+
 $resp = $r.GetResponse().GetResponseStream();
 $sr = new-object System.IO.StreamReader $resp;
 $result = $sr.ReadToEnd();
-write-host($result);
 
 if ($filesCount -gt 1) {
 	$found = $result -match '"new_album_id":"(.+?)"';
 	if ($found) {
 		$newAlbumId = $matches[1];
-		write-host('new_album_id = {0}' -f $newAlbumId);
+		# write-host('new_album_id = {0}' -f $newAlbumId);
 	};
 };
-
-
-
-write-host("You'll get a direct link to your image in clipboard when the script passes.");
-write-host("This window will close automatically. Please wait... ");
-write-host;
 
 
 
@@ -77,10 +90,13 @@ $contentTypeMap = @{
     ".tiff" = "image/tiff";
   };
 
+$isAtLeastOneImageUploaded = $false;  
+  
 for ($i = 0; $i -lt $filesCount; $i++) {
 	$file = $filesArray[$i].ToString().Trim();
 	
-	write-host $file
+	write-host;
+	write-host($file);
 	
 	$dotPosition = $file.LastIndexOf('.');
 	$slashPosition = $file.LastIndexOf('\');
@@ -95,10 +111,10 @@ for ($i = 0; $i -lt $filesCount; $i++) {
 	if ($contentTypeMap[$ext]) {
 		$contentType = $contentTypeMap[$ext]; 
 	} else {
-		write-host('Incorrect extension type');
-		# [Environment]::Exit(1);				# TODO
+		write-host('Incorrect extension type of file.');
+		continue;
+		# [Environment]::Exit(1);
 	};
-	
 	
 	$url = 'http://imgur.com/upload';
 	$r = [System.Net.WebRequest]::Create($url);
@@ -106,8 +122,8 @@ for ($i = 0; $i -lt $filesCount; $i++) {
 	$r.AutomaticDecompression = [System.Net.DecompressionMethods]::Deflate, [System.Net.DecompressionMethods]::GZip; 
 	
 	$boundary = [System.Guid]::NewGuid().ToString();
-	
-	$r.ContentType = "multipart/form-data; boundary={0}; charset=UTF-8" -f $boundary;
+
+	$r.ContentType = "multipart/form-data; boundary={0}" -f $boundary;
 	$r.Method = "POST";
 	$r.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36 OPR/36.0.2130.32";
 	$r.Accept = "application/json, text/javascript, */*; q=0.01";
@@ -119,6 +135,8 @@ for ($i = 0; $i -lt $filesCount; $i++) {
 	$r.Headers.Add("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
 	$r.Headers.Add("Origin", "http://imgur.com");
 	$r.Headers.Add("X-Requested-With", "XMLHttpRequest");
+	
+	$r.Headers.Add("Cookie", $cookieSid);
 	
 	$bytes = [System.IO.File]::ReadAllBytes($file);
 	$enc = [System.Text.Encoding]::GetEncoding(0);
@@ -181,7 +199,8 @@ for ($i = 0; $i -lt $filesCount; $i++) {
 	};
 	
 	[void]$contents.AppendLine($header);
-	[void]$contents.AppendLine('Content-Disposition: form-data; name="Filedata"; filename="{0}"' -f $fileName);
+	$fileContentDisposition = 'Content-Disposition: form-data; name="Filedata"; filename="{0}$ext"' -f $fileName, $ext;
+	[void]$contents.AppendLine($fileContentDisposition);
 	[void]$contents.AppendLine('Content-Type: {0}' -f $contentType);
 	[void]$contents.AppendLine();
 	[void]$contents.AppendLine($filedata);
@@ -202,50 +221,47 @@ for ($i = 0; $i -lt $filesCount; $i++) {
 	
 	$sr = new-object System.IO.StreamReader $resp;
 	$result = $sr.ReadToEnd();
-	write-host($result);
+	
+	write-host("Uploaded.");
+	$isAtLeastOneImageUploaded = $true;
 		
 };
 
-if ($filesCount -eq 1) {
 
-	$found = $result -match '"hash":"(.+?)"'
-	if ($found) {
-		$hash = $matches[1];
+if ($isAtLeastOneImageUploaded) {
+	
+	$date = Get-Date -UFormat "%d.%m.%y %T";
+
+	if ($filesCount -eq 1) {
+
+		$found = $result -match '"hash":"(.+?)"'
+		if ($found) {
+			$hash = $matches[1];
+		};
+		# write-host('hash = {0}' -f $hash);
+
+		$link = 'http://i.imgur.com/{0}{1}' -f $hash, $ext;
+		$logFile = 'log_single.txt';
+		$logString = "{0}       [{1}] {2}" -f $link, $date, $file;
+
+	} else {
+
+		$link = 'http://imgur.com/a/{0}' -f $newAlbumId;
+		$logFile = 'log_albums.txt';
+		$logString = "{0}       [{1}]" -f $link, $date;
 	};
-	write-host('hash = {0}' -f $hash);
 
-	$link = 'http://i.imgur.com/{0}{1}' -f $hash, $ext;
+	$stream = New-Object System.IO.StreamWriter(New-Object IO.FileStream($logFile, [System.IO.FileMode]::Append));
+	$stream.WriteLine($logString);
+	$stream.close();
+
+	write-host;
+	write-host($link);
+	write-host;
+		
+	$link | C:\Windows\System32\clip.exe;
 	
-} else {
-
-	$link = 'http://imgur.com/a/{0}' -f $newAlbumId;
-	
-};
-
-write-host;
-write-host($link);
-write-host;
-	
-$link | C:\Windows\System32\clip.exe;
+}
 
 
-<#
-
-
-
-
-
-
-
-
-$date = Get-Date -UFormat "%d.%m.%y %T";
-$logString = "{0}       [{1}] {2}" -f $link, $date, $file;
-
-$stream = New-Object System.IO.StreamWriter(New-Object IO.FileStream("log.txt", [System.IO.FileMode]::Append));
-$stream.WriteLine($logString);
-$stream.close();
-
-$link | C:\Windows\System32\clip.exe;
-
-#>
 powershell
